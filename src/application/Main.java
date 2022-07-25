90ff80486b8957a97d3c2d8a8e04ccbd1a8c706c
Wagner Alves <guinn83@hotmail.com>
2022-07-25 17:52:31 -0300

    Class Seller and Department created

---
+++ src/application/Main.java
@@ -0,0 +1,16 @@
+package application;
+
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Department dp = new Department("Books");
+
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, dp);
+
+        System.out.println(seller);
+    }
+}
