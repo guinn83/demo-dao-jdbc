b0f15ab2d70dee40fb49e880e4012deb03b17042
Wagner Alves <guinn83@hotmail.com>
2022-07-25 18:44:01 -0300

    SellerDao, DepartmentDao and DaoFactory

---
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -0,0 +1,34 @@
+package model.dao.impl;
+
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+import java.util.List;
+
+public class DepartmentDaoJDBC implements DepartmentDao {
+
+    @Override
+    public void insert(Department obj) {
+
+    }
+
+    @Override
+    public void update(Department obj) {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public Department findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Department> findAll() {
+        return null;
+    }
+}
